f32b5ff10871f3c006bf70f94b9e92c0cd1a15fc
Vyacheslav <32465843+VyBelyaev@users.noreply.github.com>
2017-10-13 11:06:47 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     int height;
+     printf("How many floors do you want:");
+     scanf("%d",&height);
+     int i;
+     int j;
+     int k;
+     int stars;
+     int spaces;
+
+     //This is pyramid
+
+     for(i = 0;i <= height; i++)
+        {
+        for(j = 0; j <= i - 1; j++){
+         printf("*");
+        }
+         printf("\n");
+        }
+     printf("\n");
+
+     for(i = 0; i < height; i++)
+        {
+        for(j = 0; j < height - i; j++ )
+            printf("*");
+        printf("\n");
+        }
+
+     //This is
+
+     for(i = 0; spaces = height , stars =- 1; i <= height; i++,stars += 2 ;spaces--)
+        {
+        for(j = 0;j < spaces;j++){
+            printf(" ");
+        }
+        for(k = 0;k < stars;k++)
+        {
+         printf("*");
+        }
+         printf("\n");
+        }
+     for(i = 0;i < height - 1;i++)
+      {
+      printf(" ");
+      }
+      printf("\n");
+}
+
+
+

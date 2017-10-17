d48bf474eef9ae51054509eff64f4dafacaf8f49
Vyacheslav <32465843+VyBelyaev@users.noreply.github.com>
2017-10-17 23:29:17 +0300

    Add files via upload

---
+++ ATM.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n [5000];
+    int i;
+    int code = 0;
+    int pin= 0;
+
+    for(;;);
+    srand(time(NULL));
+    code = rand()%5000;
+    for(i = 0;i < 3;i++)
+    {
+        for(;;)
+            {
+               printf("\nEnter your pin");
+               scanf("%d",&pin);
+                    if(pin == 0000)
+                    {
+                    printf("\nAccessed");
+                    }
+                    else if (pin == 1111)
+                    {
+                      printf("Withdraw all money from the ATM ? Yes/No");
+
+                    }
+                    int n [5000];
+                    for(i = 0;i <5000;i++)
+                    n[i] = 0;
+                    printf("%s%13s\n" , "Withdraw cash" , "View balance");
+                    for(i = 0; i  < 5000;i++)
+                    printf("%7d%13d\n" , i,n[i]);
+
+
+
+            }
+             return 0 ;
+    }
+     return 0 ;
+}

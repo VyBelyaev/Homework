a362e45b4465b3bb816899cc0f6f42d990c737fb
Vyacheslav <32465843+VyBelyaev@users.noreply.github.com>
2017-10-18 09:03:49 +0300

    Update ATM.c

--- ATM.c
+++ ATM.c
@@ -6,41 +6,62 @@ int main()
 {
     int n [5000];
     int i;
-    int code = 0;
+    int code [5000];
     int pin= 0;
 
     for(;;);
-    srand(time(NULL));
-    code = rand()%5000;
+    int code [5000];
+    for(i = 0;i <5000;i++)
+    
     for(i = 0;i < 3;i++)
     {
         for(;;)
             {
                printf("\nEnter your pin");
-               scanf("%d",&pin);
+               scanf("%d",&code);
                     if(pin == 0000)
                     {
                     printf("\nAccessed");
                     }
-                    else if (pin == 1111)
-                    {
-                      printf("Withdraw all money from the ATM ? Yes/No");
 
-                    }
-                    
                     int n [5000];
-                    for(i = 0;i <5000;i++)
+                    for(i = 0;i < 1;i++)
+                    n[i] = 0;
+                    printf("%s%13s\n" , "Withdraw cash" , "View balance");
+                    for(i = 0; i  < 1;i++)
+                    printf("%7d%13d\n" , i,n[i]);
+
+                    break ;
+
+
+
+
+            }
+            for(;;)
+            {
+               printf("\nEnter your pin");
+               scanf("%d",&pin);
+                    if(pin == 1111)
+                    {
+                    printf("\nWithdraw all money from the ATM? Yes\nNo");
+                    }
+
+                    int n [5000];
+                    for(i = 0;i < 5000;i++)
                     n[i] = 0;
                     printf("%s%13s\n" , "Withdraw cash" , "View balance");
                     for(i = 0; i  < 5000;i++)
                     printf("%7d%13d\n" , i,n[i]);
-            }        
-            return 0 ;
+
+                    break ;
+            }
+
     }
      return 0 ;
 }
-
-
+    
+    
+        
 
 
        

a53485b2becff54f81598da9c5b36b15f33d098d
Vyacheslav <32465843+VyBelyaev@users.noreply.github.com>
2017-10-19 11:26:20 +0300

    Update ATM.c

--- ATM.c
+++ ATM.c
@@ -1,63 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
+#include <time.h>
 
 int main()
 {
-    int n [5000];
-    int i;
-    int code [5000];
-    int pin = 0;
-
-    for(;;);
-    int code [5000];
-    srand (time(NULL));
-    pin = rand() %5000;
-    for(i = 0;i < 3;i++)
-    {
-        for(;;)
-            {
-               printf("\nEnter your pin");
-               scanf("%d",&pin);
-                    if(pin == 0000)
+               int pin [5000];
+               srand(time(NULL));
+               pin = rand()% 5000;
+               int code = 0;
+               printf("\nEnter your pin :");
+               scanf("%d",&code);
+                    if(code == 0000)
                     {
                     printf("\nAccessed");
-                    }
-
-                    int n [5000];
-                    for(i = 0;i < 1;i++)
+                    int n [5000] ,i;
+                    for (i = 0; i < 1;i ++)
                     n[i] = 0;
-                    printf("%s%13s\n" , "Withdraw cash" , "View balance");
-                    for(i = 0; i  < 1;i++)
-                    printf("%7d%13d\n" , i,n[i]);
+                    printf("\nWithdraw money");
+                    printf("\nFund your account");
 
-                    break ;
-
-
-
-
-            }
-            for(;;)
-            {
-               printf("\nEnter your pin");
-               scanf("%d",&pin);
-                    if(pin == 1111)
+                    printf("\nView balance");
+                    for(i =0; i < 1;i ++)
+                    printf("%7d\n" , i ,n[i]);
+                    }
+                    else if (code == 1111)
                     {
-                    printf("\nWithdraw all money from the ATM? Yes\nNo");
+                        printf("You got admin rights");
+                        int n [5000] ,i;
+                        for (i = 0; i <= 5000;i ++)
+                        n[i] = 0;
+                        printf("\nWithdraw money");
+                        printf("\nFund your account");
+                        printf("\nView balance");
+                        for(i =0; i <= 5000;i ++)
+                        printf("%7d\n" , i ,n[i]);
                     }
-
-                    int n [5000];
-                    for(i = 0;i < 5000;i++)
-                    n[i] = 0;
-                    printf("%s%13s\n" , "Withdraw cash" , "View balance");
-                    for(i = 0; i  < 5000;i++)
-                    printf("%7d%13d\n" , i,n[i]);
-
-                    break ;
-            }
-
-    }
-     return 0 ;
+                    else if(code != 0000)
+                    {
+                      printf("Access denied");
+                    }
 }
     
     

0dc32d0a2b442e0c9808a39867e565777fbe450d
Vyacheslav <32465843+VyBelyaev@users.noreply.github.com>
2017-10-27 09:08:19 +0300

    Add files via upload

---
+++ Strok.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main() {
+  char stroka[100000];
+  char sym;
+  int count;
+  int i;
+
+  printf("Enter line: ");
+  gets(stroka);
+  for(;;){
+  printf("\nEnter symbol : ");
+    sym = getchar();
+    count = 0;
+    for(i=0; stroka[i]!='\0'; i++){
+     if(stroka[i]==sym)
+       count++;
+  }
+  printf("In line\n");
+  printf("Symbol ");
+  putchar(sym); // ֲûגמה סטלגמכא
+  printf(" occurs %d once",count);
+  getchar();
+  }
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
